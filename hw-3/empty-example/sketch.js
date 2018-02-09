75ed9c63959295385454c3ea6722b697c671162b
alex <alex.killian.13@hotmail.com>
2018-02-08 20:26:05 -0700

    completed part one of hw-3

---
+++ hw-3/empty-example/sketch.js
@@ -0,0 +1,16 @@
+// this is the function setup
+function setup() {
+
+  //creating the canvas
+  createCanvas( 600, 400 );
+
+  //making the backround pink
+  background( 'pink');
+}
+
+// create my draw function
+function draw(){
+
+  //write hello world using p5 positioned at ( x: 20, y: 30 )
+  text( "Hello World!", 300, 200);
+}
