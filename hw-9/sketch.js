62774117918c68f193c459f910162a4ec0e737cf
alex <alex.killian.13@hotmail.com>
2018-04-11 18:22:59 -0600

    finished drawing started read.me

---
+++ hw-9/sketch.js
@@ -0,0 +1,47 @@
+
+
+let posArr = [];
+let yPos = 0
+let xPos = 0
+
+function setup() {
+
+  //creating the canvas
+  createCanvas( windowWidth, windowHeight );
+  background( 0);
+
+  // create numbers for the array
+  for( let num = 0; num < 10; num++){
+  posArr.push(random(0, windowWidth))
+}
+}
+
+// create my draw function
+function draw(){
+
+
+
+//add circle function into the draw function
+
+    drawCircle();
+}
+
+
+//********
+//Create function to draw an ellipse
+//*********
+function drawCircle() {
+
+
+// using the array to create more than one circle
+for( let idx = 0; idx < posArr.length; idx++){
+  stroke( 0 );
+  fill( 'rgb(251, 28, 229)');
+  ellipse( posArr[idx]  , yPos, 100 );
+
+
+  yPos = ( yPos + 1 ) % windowHeight
+
+}
+
+  }
