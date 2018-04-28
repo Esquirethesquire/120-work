3fec4d74b35b3108f84e7c661a49f67f9682b98a
alex <alex.killian.13@hotmail.com>
2018-04-28 15:38:43 -0600

    created ship and bouncing balls.

--- final-project/sketch.js
+++ final-project/sketch.js
@@ -2,13 +2,16 @@
 
 let circle = [];
 let bg_color;
-var ship;
+let ship;
+
+
 function setup() {
 
     // createCanvas(windowWidth, windowHeight);
     createCanvas(windowWidth, windowHeight);
     bg_color = color('rgb(0, 0, 0)');
-//    ship = new Ship();
+
+    ship = new Ship();
 
     for( let i = 0; i < 5; i++) {
 
@@ -23,24 +26,64 @@ function setup() {
 function draw() {
     background(bg_color);
 
+    ship.show();
+    ship.turn();
+
+
+
     for(let i = 0; i < circle.length; i++){
       circle[i].frame();
+
     }
-//    this.render();
+
+
 
 
   }
 
-  //create ship
-//class Ship {
-//    constructor() {
-//      this.pos = createVector(width/2, height/2);
-//      this.rad = 10;
-//    }
-//    render() {
-//        triangle(-this.rad, this.rad. this.rad, this.rad, 0, -this.rad);
-//    }
-//  }
+  //************
+  //create ship class
+  //**********
+  class Ship {
+
+    constructor(){
+      this.pos = createVector(width/2, height/2);
+      this.r = 20;
+      this.direction = 0;
+      this.rotation = 0;
+    }
+
+    show(){
+
+      rotate(this.direction);
+      stroke(255);
+      strokeWeight(5);
+      fill('rgb(0, 158, 6)');
+      triangle(-this.r,this.r,this.r,this.r,0,-this.r);
+      }
+
+  //adding a rotate mechanic
+    turn(){
+      this.direction += this.rotation;
+    }
+
+    setRotation(a){
+      this.rotation = a;
+    }
+
+    keyPressed(){
+      if (keyCode == RIGHT_ARROW) {
+        ship.setRotation(0.1);
+      } else if (keyCode == LEFT_ARROW);
+      ship.setRotation(-0.1);
+    }
+
+    keyReleased() {
+        ship.setRotation(0);
+    }
+
+  }
+
 
 
 //******************
@@ -51,7 +94,7 @@ class Orb {
   constructor(x,y,s) {
     this.x = x;
     this.y = y;
-    this.s = 200;
+    this.s = 100;
     this.r = this.s/2;
     this.deltaX = random(-5, 5);
     this.deltaY = random(-5, 5);
@@ -66,9 +109,10 @@ class Orb {
   }
 
   show() {
+
     stroke(255);
     strokeWeight(5);
-    fill('rgb(0, 175, 2)');
+    fill('rgb(212, 0, 178)');
     ellipse(this.x,this.y,this.s)
     }
 
