4bfdd0c5104704c59521db8a13dffad063f95dd7
alex <alex.killian.13@hotmail.com>
2018-05-07 19:19:39 -0600

    finish read me/ change file name

--- final-project-final/sketch.js
+++ final-project-final/sketch.js
@@ -1,7 +1,86 @@
+let bg_color;
+let ships = [];
+let orbs = [];
+
 function setup() {
-  // put setup code here
+// createCanvas(windowWidth, windowHeight);
+    createCanvas(windowWidth, windowHeight);
+    bg_color = color('rgb(0, 0, 0)');
+
+    for( let i = 0; i < 1; i++) {
+      ships.push(new Ship());
 }
+    let init_x = 200
+    let init_y = 200
+    for( let i = 0; i < 5; i++) {
+
+    orbs.push(new Orb(init_x, init_y));
+    init_x +=400;
+    if(init_x > width){
+      init_x = 200
+      init_y +=200
+    }
+    }
+
+  }
+
+
+
 
 function draw() {
-  // put drawing code here
+  //adding text for controls
+    background(bg_color);
+    textSize(18);
+    fill(255);
+    text("Controls: Arrow keys to turn and move", 50,50);
+    text("DONT GET HIT!", 50,100);
+
+    for(let i = 0; i < ships.length; i++){
+    ships[0].show();
+    ships[0].turn();
+    ships[0].update();
+    ships[0].edges();
+
+  }
+
+
+    for(let i = 0; i < orbs.length; i++){
+      orbs[i].frame();
+
+//making the collision with orbs and ship make ship disappear
+        for(let j = 0; j < ships.length; j++){
+          if(orbs[i].hits(ships[0])){
+            ships.splice(j, 1);
+
+
+      }
+
+    }
+  }
+}
+
+
+
+
+
+    //function to make rotation and velocity stop when key is released
+    function keyReleased() {
+      ships[0].setRotation(0);
+      ships[0].boosting(false);
+
+    }
+
+
+    //function to rotate when key is pressed
+    function keyPressed() {
+
+     if (keyCode == RIGHT_ARROW) {
+        ships[0].setRotation(.1);
+      }
+      else if (keyCode == LEFT_ARROW){
+        ships[0].setRotation(-.1);
+    }
+      else if (keyCode == UP_ARROW){
+        ships[0].boosting(true);
+  }
 }
